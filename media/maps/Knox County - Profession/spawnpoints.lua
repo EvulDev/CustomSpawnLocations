88b35e363a5c791b223b915ab8f1a2dcddc9e269
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-07-30 00:23:53 +0200

    Added Separate Prison spawn Added Additional spawn for Doctors, Nurses, and Burger flippers.

--- media/maps/Knox County - Profession/spawnpoints.lua
+++ media/maps/Knox County - Profession/spawnpoints.lua
@@ -12,9 +12,10 @@ function SpawnPoints()
         },
 
         policeofficer = {
-        -- Total Locations = 46, 100%
+        -- Total Locations = 62, 100%
         
-        -- Muldraugh - Police Station (10 locations, 22% spawn chance)
+        -- Muldraugh - Police Station (11 locations, 18% spawn chance)
+            {worldX=35, worldY=34, posX=134, posY=215},         -- Large entrance Office
             {worldX=35, worldY=34, posX=134, posY=215},         -- Large entrance Office
             {worldX=35, worldY=34, posX=134, posY=215},         -- Large entrance Office
             {worldX=35, worldY=34, posX=134, posY=215},         -- Large entrance Office
@@ -26,7 +27,7 @@ function SpawnPoints()
             {worldX=35, worldY=34, posX=140, posY=202},         -- Looker room
             {worldX=35, worldY=34, posX=140, posY=202},         -- Looker room
             
-        -- West Point - Police Station (20 locations, 43% spawn chance)
+        -- West Point - Police Station (20 locations, 32% spawn chance)
             {worldX=39, worldY=23, posX=206, posY=49},          -- Behind the entrance desk
             {worldX=39, worldY=23, posX=206, posY=49},          -- Behind the entrance desk
             {worldX=39, worldY=23, posX=506, posY=40},          -- Large Office
@@ -48,7 +49,24 @@ function SpawnPoints()
             {worldX=39, worldY=23, posX=196, posY=37},          -- Looker room
             {worldX=39, worldY=23, posX=196, posY=37},          -- Looker room
             
-        -- Rosewood KN Prison (16 Locations, 35% spawn chance)
+        -- Rosewood KN Police Station (15 Locations, 24% spawn chance)
+            {worldX=26, worldY=39, posX=274, posY=39},          -- Behind the entrance desk
+            {worldX=26, worldY=39, posX=274, posY=39},          -- Behind the entrance desk
+            {worldX=26, worldY=39, posX=273, posY=29},          -- Large office
+            {worldX=26, worldY=39, posX=273, posY=29},          -- Large office
+            {worldX=26, worldY=39, posX=273, posY=29},          -- Large office
+            {worldX=26, worldY=39, posX=268, posY=35},          -- Large office (pos2)
+            {worldX=26, worldY=39, posX=268, posY=35},          -- Large office (pos2)
+            {worldX=26, worldY=39, posX=268, posY=35},          -- Large office (pos2)
+            {worldX=26, worldY=39, posX=273, posY=23},          -- Small office
+            {worldX=26, worldY=39, posX=273, posY=23},          -- Small office
+            {worldX=26, worldY=39, posX=266, posY=29},          -- Conference Room
+            {worldX=26, worldY=39, posX=266, posY=29},          -- Conference Room
+            {worldX=26, worldY=39, posX=266, posY=29},          -- Conference Room
+            {worldX=26, worldY=39, posX=249, posY=26},          -- Locker Room (Left)
+            {worldX=26, worldY=39, posX=256, posY=26},          -- Locker Room (Right)
+            
+        -- Rosewood KN Prison (16 Locations, 26% spawn chance)
             {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
             {worldX=25, worldY=39, posX=247, posY=200},         -- Brake Room (Guardhouse)
             {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
@@ -68,16 +86,18 @@ function SpawnPoints()
             {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
         },
         doctor = {
-        -- Muldraugh - Cortman Medical
+        -- Total Locations = 21, 100%
+        
+        -- Muldraugh - Cortman Medical (4 Locations, 19% spawn chance)
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             
-        -- West Point - Pharmahug
+        -- West Point - Pharmahug (1 Locations, 5% spawn chance)
             {worldX=39, worldY=22, posX=231, posY=197},         -- Behind the counter
             
-        -- West Point - Dentist
+        -- West Point - Dentist (6 Locations, 29% spawn chance)
             {worldX=39, worldY=22, posX=181, posY=289},         -- Reception
             {worldX=39, worldY=22, posX=182, posY=283},         -- Medical Room 0
             {worldX=39, worldY=22, posX=182, posY=283},         -- Medical Room 0
@@ -85,7 +105,7 @@ function SpawnPoints()
             {worldX=39, worldY=22, posX=188, posY=286},         -- Medical Room 1
             {worldX=39, worldY=22, posX=188, posY=280},         -- Office
             
-        -- Rosewood Medical
+        -- Rosewood Medical (7 Locations, 33% spawn chance)
             --{worldX=33, worldY=42, posX=254, posY=158},       -- Reception
             {worldX=33, worldY=42, posX=255, posY=150},         -- Medical room 0
             {worldX=33, worldY=42, posX=265, posY=153},         -- Medical room 1
@@ -94,20 +114,27 @@ function SpawnPoints()
             {worldX=33, worldY=42, posX=271, posY=155},         -- Office
             {worldX=33, worldY=42, posX=254, posY=155},         -- Large Office
             {worldX=33, worldY=42, posX=254, posY=155},         -- Large Office
+            
+        -- Rosewood - Prison (3 Locations, 14% spawn chance)
+            {worldX=25, worldY=39, posX=206, posY=164, posZ=1}, -- Prison Infirmary Room 1
+            {worldX=25, worldY=39, posX=211, posY=164, posZ=1}, -- Prison Infirmary Room 2
+            {worldX=25, worldY=39, posX=199, posY=161, posZ=1}, -- Prison Infirmary Storage
         },
         nurse = {
-            -- Muldraugh - Cortman Medical
+        -- Total Locations = 23, 100%
+        
+        -- Muldraugh - Cortman Medical (4 Locations, 17% spawn chance)
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             {worldX=36, worldY=33, posX=77, posY=129},          -- Medical room
             
-            -- West Point - Pharmahug
+        -- West Point - Pharmahug (3 Locations, 13% spawn chance)
             {worldX=39, worldY=22, posX=231, posY=197},         -- Behind the counter
             {worldX=39, worldY=22, posX=231, posY=197},         -- Behind the counter
             {worldX=39, worldY=22, posX=231, posY=197},         -- Behind the counter
             
-            -- West Point - Dentist
+        -- West Point - Dentist (5 Locations, 22% spawn chance)
             {worldX=39, worldY=22, posX=181, posY=289},         -- Reception
             {worldX=39, worldY=22, posX=181, posY=289},         -- Reception
             {worldX=39, worldY=22, posX=181, posY=289},         -- Reception
@@ -115,7 +142,7 @@ function SpawnPoints()
             {worldX=39, worldY=22, posX=188, posY=286},         -- Medical Room 1
             --{worldX=39, worldY=22, posX=188, posY=280},       -- Office
             
-            -- Rosewood - Medical
+        -- Rosewood - Medical (8 Locations, 35% spawn chance)
             {worldX=33, worldY=42, posX=254, posY=158},         -- Reception
             {worldX=33, worldY=42, posX=254, posY=158},         -- Reception
             {worldX=33, worldY=42, posX=254, posY=158},         -- Reception
@@ -124,6 +151,63 @@ function SpawnPoints()
             {worldX=33, worldY=42, posX=270, posY=159},         -- Medical room 2
             {worldX=33, worldY=42, posX=271, posY=155},         -- Office
             {worldX=33, worldY=42, posX=254, posY=155},         -- Large Office
+            
+        -- Rosewood - Prison (3 Locations, 13% spawn chance)
+            {worldX=25, worldY=39, posX=206, posY=164, posZ=1}, -- Prison Infirmary Room 1
+            {worldX=25, worldY=39, posX=211, posY=164, posZ=1}, -- Prison Infirmary Room 2
+            {worldX=25, worldY=39, posX=199, posY=161, posZ=1}, -- Prison Infirmary Storage
+        },
+        fireofficer = {
+        -- Total Locations = 4, 100%
+            {worldX=27, worldY=39, posX=37, posY=31},           -- Conference Room
+            {worldX=27, worldY=39, posX=41, posY=46},           -- Large Office
+            {worldX=27, worldY=39, posX=41, posY=46, posZ=1},   -- Brake Room
+            {worldX=27, worldY=39, posX=30, posY=30, posZ=1},   -- Locker Room
+        },
+
+        burgerflipper = {
+        -- Total Locations = 27, 100%
+        
+        -- Valley Station (2 Locations, 7% spawn chance)
+            {worldX=46, worldY=16, posX=171, posY=65},           -- Valley Station - Burger Restaurant
+            {worldX=42, worldY=19, posX=148, posY=16},           -- Valley Station - Niner
+            
+        -- Valley Station Mall (5 Locations, 19% spawn chance)
+            {worldX=46, worldY=19, posX=154, posY=76},           -- Valley Station - Mall - Burger place
+            {worldX=46, worldY=19, posX=79, posY=66},            -- Valley Station - Mall - Pizza Whirled
+            {worldX=46, worldY=19, posX=84, posY=55},            -- Valley Station - Mall - I<3Wokerolls
+            {worldX=46, worldY=19, posX=78, posY=74},            -- Valley Station - Mall - Jolley GooD
+            {worldX=46, worldY=19, posX=77, posY=84},            -- Valley Station - Mall - Tac odel Pacho
+            
+        -- West Point (4 Locations, 15% spawn chance)
+            {worldX=39, worldY=22, posX=282, posY=208},          -- West Point Spiffo's
+            {worldX=39, worldY=22, posX=282, posY=208},          -- West Point Spiffo's
+            {worldX=39, worldY=22, posX=282, posY=208},          -- West Point Spiffo's
+            {worldX=39, worldY=22, posX=282, posY=208},          -- West Point Spiffo's
+
+        -- West Point Road (2 Locations, 7% spawn chance)
+            {worldX=38, worldY=27, posX=274, posY=199},          -- West Point Road Spiffo's
+            {worldX=38, worldY=29, posX=64, posY=113},           -- West Point Road Diner
+            
+        -- Muldraugh (6 Locations, 22% spawn chance)
+            {worldX=35, worldY=32, posX=130, posY=60},           -- Muldraugh Spiffo's
+            {worldX=35, worldY=32, posX=130, posY=60},           -- Muldraugh Spiffo's
+            {worldX=35, worldY=33, posX=107, posY=210},          -- Muldraugh Pizza Whirled
+            {worldX=35, worldY=33, posX=107, posY=210},          -- Muldraugh Pizza Whirled
+            {worldX=35, worldY=35, posX=118, posY=59},           -- Muldraugh Diner
+            {worldX=35, worldY=35, posX=118, posY=59},           -- Muldraugh Diner
+            
+        -- Muldraugh Road (2 Locations, 7% spawn chance)
+            {worldX=33, worldY=37, posX=208, posY=53},           -- Muldraugh Road Diner
+            {worldX=33, worldY=37, posX=208, posY=53},           -- Muldraugh Road Diner
+            
+        -- Rosewood (6 Locations, 22% spawn chance)
+            {worldX=26, worldY=37, posX=270, posY=211},          -- Rosewood Pizza Whirled
+            {worldX=26, worldY=37, posX=270, posY=211},          -- Rosewood Pizza Whirled
+            {worldX=26, worldY=37, posX=270, posY=211},          -- Rosewood Pizza Whirled
+            {worldX=26, worldY=37, posX=267, posY=240},          -- Rosewood Spiffo's
+            {worldX=26, worldY=37, posX=267, posY=240},          -- Rosewood Spiffo's
+            {worldX=26, worldY=37, posX=267, posY=240},          -- Rosewood Spiffo's
         },
     }
 end
---
+++ media/maps/Rosewood, KY - Prison/spawnpoints.lua
@@ -0,0 +1,229 @@
+-- { name = Rosewood, KY - Prison", file = "media/maps/Rosewood, KY - Prison/spawnregions.lua" },
+
+function SpawnPoints()
+    return {
+        unemployed = {
+        -- Cell block (North East)
+            {worldX=25, worldY=39, posX=180, posY=155},
+            {worldX=25, worldY=39, posX=180, posY=152},
+            {worldX=25, worldY=39, posX=180, posY=149},
+            {worldX=25, worldY=39, posX=180, posY=146},
+            {worldX=25, worldY=39, posX=180, posY=143},
+            {worldX=25, worldY=39, posX=180, posY=140},
+            {worldX=25, worldY=39, posX=180, posY=137},
+            {worldX=25, worldY=39, posX=180, posY=134},
+            {worldX=25, worldY=39, posX=180, posY=131},
+            {worldX=25, worldY=39, posX=180, posY=128},
+            {worldX=25, worldY=39, posX=180, posY=125},
+            {worldX=25, worldY=39, posX=180, posY=122},
+            {worldX=25, worldY=39, posX=180, posY=119},
+            
+            {worldX=25, worldY=39, posX=196, posY=155},
+            {worldX=25, worldY=39, posX=196, posY=152},
+            {worldX=25, worldY=39, posX=196, posY=149},
+            {worldX=25, worldY=39, posX=196, posY=146},
+            {worldX=25, worldY=39, posX=196, posY=143},
+            {worldX=25, worldY=39, posX=196, posY=140},
+            {worldX=25, worldY=39, posX=196, posY=137},
+            {worldX=25, worldY=39, posX=196, posY=134},
+            {worldX=25, worldY=39, posX=196, posY=131},
+            {worldX=25, worldY=39, posX=196, posY=128},
+            {worldX=25, worldY=39, posX=196, posY=125},
+            {worldX=25, worldY=39, posX=196, posY=122},
+            {worldX=25, worldY=39, posX=196, posY=119},
+
+            {worldX=25, worldY=39, posX=180, posY=152, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=149, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=146, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=143, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=140, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=137, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=134, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=131, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=128, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=125, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=122, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=119, posZ=1},
+
+            {worldX=25, worldY=39, posX=196, posY=155, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=152, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=149, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=146, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=143, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=140, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=137, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=134, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=131, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=128, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=125, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=122, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=119, posZ=1},
+            
+        -- Cell block (South West)
+            {worldX=25, worldY=39, posX=180, posY=208},
+            {worldX=25, worldY=39, posX=180, posY=211},
+            {worldX=25, worldY=39, posX=180, posY=214},
+            {worldX=25, worldY=39, posX=180, posY=217},
+            {worldX=25, worldY=39, posX=180, posY=220},
+            {worldX=25, worldY=39, posX=180, posY=223},
+            {worldX=25, worldY=39, posX=180, posY=226},
+            {worldX=25, worldY=39, posX=180, posY=229},
+            {worldX=25, worldY=39, posX=180, posY=232},
+            {worldX=25, worldY=39, posX=180, posY=235},
+            {worldX=25, worldY=39, posX=180, posY=238},
+            {worldX=25, worldY=39, posX=180, posY=241},
+            {worldX=25, worldY=39, posX=180, posY=244},
+            
+            {worldX=25, worldY=39, posX=196, posY=208},
+            {worldX=25, worldY=39, posX=196, posY=211},
+            {worldX=25, worldY=39, posX=196, posY=214},
+            {worldX=25, worldY=39, posX=196, posY=217},
+            {worldX=25, worldY=39, posX=196, posY=220},
+            {worldX=25, worldY=39, posX=196, posY=223},
+            {worldX=25, worldY=39, posX=196, posY=226},
+            {worldX=25, worldY=39, posX=196, posY=229},
+            {worldX=25, worldY=39, posX=196, posY=232},
+            {worldX=25, worldY=39, posX=196, posY=235},
+            {worldX=25, worldY=39, posX=196, posY=238},
+            {worldX=25, worldY=39, posX=196, posY=241},
+            {worldX=25, worldY=39, posX=196, posY=244},
+            
+            {worldX=25, worldY=39, posX=180, posY=196, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=199, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=202, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=205, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=208, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=211, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=214, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=217, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=220, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=223, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=226, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=229, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=232, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=235, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=238, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=241, posZ=1},
+            {worldX=25, worldY=39, posX=180, posY=244, posZ=1},
+
+            {worldX=25, worldY=39, posX=196, posY=208, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=211, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=214, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=217, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=220, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=223, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=226, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=229, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=232, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=235, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=238, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=241, posZ=1},
+            {worldX=25, worldY=39, posX=196, posY=244, posZ=1},
+        },
+
+        policeofficer = {
+        -- Rosewood KN Prison
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Brake Room (Guardhouse)
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Brake Room (Guardhouse)
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Brake Room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            
+            {worldX=25, worldY=39, posX=208, posY=177},         -- Reception
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Visitation Reception
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Visitation Reception
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Surveillance
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Surveillance
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Looker room
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Looker room
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=198, posY=199, posZ=1}, -- Brake Room (2nd floor)
+            {worldX=25, worldY=39, posX=198, posY=199, posZ=1}, -- Brake Room (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=161, posZ=1}, -- Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=161, posZ=1}, -- Office (2nd floor)
+            {worldX=25, worldY=39, posX=189, posY=185, posZ=1}, -- Surveillance (2nd floor)
+            {worldX=25, worldY=39, posX=189, posY=185, posZ=1}, -- Surveillance (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+        },
+        securityguard = {
+        -- Rosewood KN Prison
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=26, worldY=39, posX=23, posY=187},          -- Gate House
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Break Room (Guardhouse)
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Break Room (Guardhouse)
+            {worldX=25, worldY=39, posX=247, posY=200},         -- Break Room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Surveillance (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=250, posY=195},         -- Looker room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            {worldX=25, worldY=39, posX=246, posY=198, posZ=1}, -- Conference room (Guardhouse)
+            
+            {worldX=25, worldY=39, posX=208, posY=177},         -- Reception
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Reception Office
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Visitation Reception
+            {worldX=25, worldY=39, posX=197, posY=180},         -- Visitation Reception
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Surveillance
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Surveillance
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Looker room
+            {worldX=25, worldY=39, posX=175, posY=165},         -- Prison Looker room
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=193, posY=163},         -- Large Office
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=181, posY=159, posZ=1}, -- Large Office (2nd floor)
+            {worldX=25, worldY=39, posX=198, posY=199, posZ=1}, -- Brake Room (2nd floor)
+            {worldX=25, worldY=39, posX=198, posY=199, posZ=1}, -- Brake Room (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=161, posZ=1}, -- Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=161, posZ=1}, -- Office (2nd floor)
+            {worldX=25, worldY=39, posX=189, posY=185, posZ=1}, -- Surveillance (2nd floor)
+            {worldX=25, worldY=39, posX=189, posY=185, posZ=1}, -- Surveillance (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+            {worldX=25, worldY=39, posX=183, posY=184, posZ=1}, -- Surveillance Office (2nd floor)
+        },
+        
+        doctor = {
+            {worldX=25, worldY=39, posX=206, posY=164, posZ=1}, -- Prison Infirmary Room 1
+            {worldX=25, worldY=39, posX=206, posY=164, posZ=1}, -- Prison Infirmary Room 1
+            {worldX=25, worldY=39, posX=211, posY=164, posZ=1}, -- Prison Infirmary Room 2
+            {worldX=25, worldY=39, posX=211, posY=164, posZ=1}, -- Prison Infirmary Room 2
+            {worldX=25, worldY=39, posX=199, posY=161, posZ=1}, -- Prison Infirmary Storage
+        },
+    }
+end
