4b21d8f96b15316ecda79201a5638fc04a010b41
ColdEvul <andreas.brostrom.ce@gmail.com>
2017-08-02 23:56:38 +0200

    Added a couple of new spawnpoint selections

---
+++ media/maps/Knox County - Hotel/spawnpoints.lua
@@ -0,0 +1,76 @@
+-- { name = "Knox County, KY - In Your Hotel Room", file = "media/maps/Knox County - Hotel/spawnregions.lua" },
+
+function SpawnPoints()
+    return {
+        unemployed = {
+        -- Muldraugh - Tavern
+            {worldX=35, worldY=35, posX=267, posY=55, posZ=1},
+            {worldX=35, worldY=35, posX=264, posY=55, posZ=1},
+            {worldX=35, worldY=35, posX=267, posY=46, posZ=1},
+            {worldX=35, worldY=35, posX=264, posY=46, posZ=1},
+            {worldX=35, worldY=35, posX=261, posY=46, posZ=1},
+            
+        -- Muldraugh - Sunstar Hotel
+            {worldX=35, worldY=32, posX=130, posY=230},
+            {worldX=35, worldY=32, posX=136, posY=229},
+            {worldX=35, worldY=32, posX=146, posY=227},
+            {worldX=35, worldY=32, posX=146, posY=221},
+            {worldX=35, worldY=32, posX=146, posY=209},
+            {worldX=35, worldY=32, posX=146, posY=201},
+            {worldX=35, worldY=32, posX=138, posY=201},
+            {worldX=35, worldY=32, posX=131, posY=201},
+            {worldX=35, worldY=32, posX=125, posY=201},
+            {worldX=35, worldY=32, posX=130, posY=230, posZ=1},
+            {worldX=35, worldY=32, posX=135, posY=230, posZ=1},
+            {worldX=35, worldY=32, posX=140, posY=230, posZ=1},
+            {worldX=35, worldY=32, posX=146, posY=227, posZ=1},
+            {worldX=35, worldY=32, posX=146, posY=221, posZ=1},
+            {worldX=35, worldY=32, posX=146, posY=209, posZ=1},
+            {worldX=35, worldY=32, posX=146, posY=203, posZ=1},
+            {worldX=35, worldY=32, posX=146, posY=203, posZ=1},
+            {worldX=35, worldY=32, posX=138, posY=201, posZ=1},
+            {worldX=35, worldY=32, posX=131, posY=201, posZ=1},
+            {worldX=35, worldY=32, posX=122, posY=201, posZ=1},
+            {worldX=35, worldY=32, posX=116, posY=200, posZ=1},
+            
+        -- Muldraugh - Motel
+            {worldX=36, worldY=32, posX=114, posY=158},
+            {worldX=36, worldY=32, posX=111, posY=158},
+            {worldX=36, worldY=32, posX=107, posY=158},
+            {worldX=36, worldY=32, posX=103, posY=158},
+            {worldX=36, worldY=32, posX=114, posY=158, posZ=1},
+            {worldX=36, worldY=32, posX=111, posY=158, posZ=1},
+            {worldX=36, worldY=32, posX=107, posY=158, posZ=1},
+            {worldX=36, worldY=32, posX=103, posY=158, posZ=1},
+            
+        -- West Point - Mini Hotel
+            {worldX=44, worldY=23, posX=44, posY=15},
+            {worldX=44, worldY=23, posX=37, posY=15},
+            {worldX=44, worldY=23, posX=31, posY=15},
+            {worldX=44, worldY=23, posX=25, posY=15},
+            {worldX=44, worldY=23, posX=20, posY=15},
+            {worldX=44, worldY=23, posX=24, posY=24},
+            {worldX=44, worldY=23, posX=30, posY=24},
+            {worldX=44, worldY=23, posX=36, posY=24},
+            {worldX=44, worldY=23, posX=42, posY=24},
+            
+        -- Rosewood KN - Motel
+            {worldX=26, worldY=38, posX=285, posY=8},
+            {worldX=26, worldY=38, posX=281, posY=8},
+            {worldX=26, worldY=38, posX=278, posY=8},
+            {worldX=26, worldY=38, posX=274, posY=8},
+            {worldX=26, worldY=38, posX=285, posY=8, posZ=1},
+            {worldX=26, worldY=38, posX=281, posY=8, posZ=1},
+            {worldX=26, worldY=38, posX=278, posY=8, posZ=1},
+            {worldX=26, worldY=38, posX=274, posY=8, posZ=1},
+            {worldX=26, worldY=38, posX=268, posY=24, posZ=1},
+            
+        -- Rosewood KN - Tavern
+            {worldX=26, worldY=38, posX=216, posY=33, posZ=1},
+            {worldX=26, worldY=38, posX=219, posY=33, posZ=1},
+            {worldX=26, worldY=38, posX=222, posY=33, posZ=1},
+            {worldX=26, worldY=38, posX=225, posY=33, posZ=1},
+            {worldX=26, worldY=38, posX=228, posY=33, posZ=1},
+        },
+    }
+end
---
+++ media/maps/West Point, KY - Hotel/spawnpoints.lua
@@ -0,0 +1,18 @@
+-- { name = "West Point, KY - In Your Hotel Room", file = "media/maps/West Point, KY - Hotel/spawnregions.lua" },
+
+function SpawnPoints()
+    return {
+        unemployed = {
+        -- West Point - Mini Hotel
+            {worldX=44, worldY=23, posX=44, posY=15},
+            {worldX=44, worldY=23, posX=37, posY=15},
+            {worldX=44, worldY=23, posX=31, posY=15},
+            {worldX=44, worldY=23, posX=25, posY=15},
+            {worldX=44, worldY=23, posX=20, posY=15},
+            {worldX=44, worldY=23, posX=24, posY=24},
+            {worldX=44, worldY=23, posX=30, posY=24},
+            {worldX=44, worldY=23, posX=36, posY=24},
+            {worldX=44, worldY=23, posX=42, posY=24},
+        },
+    }
+end
